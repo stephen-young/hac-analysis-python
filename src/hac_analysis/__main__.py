cf190720ac610792f70b4a9359fae512544808bf
Stephen Young <stephen.m.young@proton.me>
2026-06-11 00:31:22 -0400

    Remove hydra dependency
    Hydra configuration management library is nice but it is overkill for
    what this library needs. I have switched to switched to layered approach
    where the default config is defined in a dictionary in `__main__.py` and
    the values can be changed with a `config.toml` file in the root project
    directory. Current plan is to make it possible for the user to define
    the path of this config file through the CLI.

--- src/hac_analysis/__main__.py
+++ src/hac_analysis/__main__.py
@@ -1,16 +1,46 @@
-import hydra
+import tomllib
+import pprint
 from pathlib import Path
-from omegaconf import DictConfig, OmegaConf
+from .metadata import load_metadata
+
+# TODO: Add command-line interface
+
+# Default values for configuration parameters
+CONFIG_PATH = "config.toml"  # TODO: expose config path file in CLI
+DEFAULT_CONFIG = {
+    "paths": {"metadata_dir": "./metadata"},
+    "metadata": {
+        "benchmark": "benchmark_test_index.json",
+        "calibration": "calibration_constants.json",
+        "constants": "analysis_constants.json",
+        "database": "database.json",
+    },
+}
 
 
-@hydra.main(version_base=None, config_path="conf", config_name="config")
-def main(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+def main() -> None:
     print(Path.cwd())
+    config = DEFAULT_CONFIG
+    config_file = Path(CONFIG_PATH)
+    if config_file.exists():
+        file_config = tomllib.loads(config_file.read_text())
+        config = merge_config(config, file_config)
+    pprint.pp(config)
+    data = load_metadata(config["paths"]["metadata_dir"], config["metadata"])
+    pprint.pp(data["benchmark"][0])
 
-    db_index_loc = Path(cfg.paths.metadata_dir) / cfg.metadata.database
-    print(db_index_loc)
-    print(db_index_loc.exists())
+
+def merge_config(original: dict, new: dict) -> dict:
+    # Merge default config and values from config file
+    #   Recursively searches dictionaries to override default values with values in config file
+
+    for key in original.keys():
+        if isinstance(original[key], dict) and key in new:
+            original[key] = merge_config(original[key], new[key])
+        else:
+            original[key] = new[key] if key in new else original[key]
+
+    return original
 
 
 if __name__ == "__main__":
